a16080ad46e9bea99581292975c48ae26e45221e
Ömer Yirmibeş <92225729+0mer25@users.noreply.github.com>
2022-10-12 19:10:47 +0300

    Add files via upload

---
+++ NameGenerator.py
@@ -0,0 +1,10 @@
+# Project_1
+# NameGenerator
+
+import random
+
+names = ["A" , "B" , "C " , "D"]
+surnames = ["1" , "2" , "3" , "4"]
+
+person = random.choice(names) + random.choice(surnames) 
+print(person)

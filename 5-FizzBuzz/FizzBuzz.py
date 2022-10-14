69f8825c553b77098dfd6d574ef1c1b4b25a27b5
Ömer Yirmibeş <2110213001@ogrenci.karabuk.edu.tr>
2022-10-14 12:43:50 +0300

    add FizzBuzz and RockPaperScissors

---
+++ 5-FizzBuzz/FizzBuzz.py
@@ -0,0 +1,13 @@
+# Project_5
+# FizzBuzz
+
+for i in range(30):
+    if(((i % 3) == 0) and ((i % 5) == 0)):
+        print("FizzBuzz")
+    else:
+        if((i % 3) == 0):
+            print("Fizz")
+        elif((i % 5) == 0):
+            print("Buzz")
+        else:
+            print(i)        
---
+++ 6-RockPaperScissors/RockPaperScissors.py
@@ -0,0 +1,64 @@
+# Project_6
+# Rock-Paper-Scissors
+
+import random
+
+choices = ["rock" , "paper" , "scissors"]
+
+user_point = 0
+pc_point = 0
+
+
+
+def Check(x , y):
+    global user_point,  pc_point
+    if(x == "1"):
+        if(y == "rock"):
+            print("Rock - Rock\n Draw " + str(user_point) + " - " + str(pc_point))
+        elif(y == "paper"):
+            pc_point += 1
+            print("Rock - Paper\n " + str(user_point) + " - " + str(pc_point))
+        elif(y == "scissors"):
+            user_point += 1
+            print("Rock - Scissors\n " + str(user_point) + " - " + str(pc_point))
+        else:
+            print("You entered a invalid value")
+    elif(x == "2"):
+        if(y == "rock"):
+            user_point += 1
+            print("Paper - Rock\n " + str(user_point) + " - " + str(pc_point))
+        elif(y == "paper"):
+            print("Paper - Paper\n Draw " + str(user_point) + " - " + str(pc_point))
+        elif(y == "scissors"):
+            pc_point += 1
+            print("Paper - Scissors\n " + str(user_point) + " - " + str(pc_point))
+        else:
+            print("You entered a invalid value")       
+    elif(x == "3"):
+        if(y == "rock"):
+            pc_point += 1
+            print("Scissors - Rock\n " + str(user_point) + " - " + str(pc_point))
+        elif(y == "paper"):
+            user_point += 1
+            print("Scissors - Paper\n " + str(user_point) + " - " + str(pc_point))
+        elif(y == "scissors"):
+            print("Scissors - Scissors\n Draw " + str(user_point) + " - " + str(pc_point))     
+        else:
+            print("You entered a invalid value")
+    else:
+            print("You entered a invalid value")                              
+
+while(True):
+    pc = random.choice(choices)
+    user = input("1-Rock   2-Paper   or   3-Scissors\n Choice : ")
+    print(user)
+    print(pc)
+    Check(user , pc)
+    if(user_point == 3):
+        print("You Won")
+        break
+    elif(pc_point == 3):
+        print("You Lose")
+        break
+    else:
+        continue
